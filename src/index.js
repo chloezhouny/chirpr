63a86bfb7c799af330d7e02a5d297fb2354262bf
chloezhouny <chloezhouny@gmail.com>
2022-07-18 10:04:52 -0700

    feat: 🎸 eslint config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import Login from "./pages/Login";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Login from './pages/Login';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Login />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,35 +1,38 @@
-import { Button, Input, Form, Dialog } from "antd-mobile";
+import {
+  Button, Input, Form, Dialog,
+} from 'antd-mobile';
 
-import "./index.css";
-const Login = () => {
-	const [form] = Form.useForm();
-	const onSubmit = () => {
-		const values = form.getFieldsValue();
-		Dialog.alert({
-			content: <pre>{JSON.stringify(values, null, 2)}</pre>,
-		});
-	};
-	return (
-		<div className="login">
-			<Form
-				form={form}
-				layout="horizontal"
-				mode="card"
-				footer={
-					<Button block color="primary" onClick={onSubmit} size="large">
-						Log in
-					</Button>
-				}
-			>
-				<Form.Item name="username">
-					<Input placeholder="Phone, email, or username" />
-				</Form.Item>
-				<Form.Item name="password">
-					<Input placeholder="Password" clearable type="password" />
-				</Form.Item>
-			</Form>
-		</div>
-	);
-};
+import './index.css';
+
+function Login() {
+  const [form] = Form.useForm();
+  const onSubmit = () => {
+    const values = form.getFieldsValue();
+    Dialog.alert({
+      content: <pre>{JSON.stringify(values, null, 2)}</pre>,
+    });
+  };
+  return (
+    <div className="login">
+      <Form
+        form={form}
+        layout="horizontal"
+        mode="card"
+        footer={(
+          <Button block color="primary" onClick={onSubmit} size="large">
+            Log in
+          </Button>
+   )}
+      >
+        <Form.Item name="username">
+          <Input placeholder="Phone, email, or username" />
+        </Form.Item>
+        <Form.Item name="password">
+          <Input placeholder="Password" clearable type="password" />
+        </Form.Item>
+      </Form>
+    </div>
+  );
+}
 
 export default Login;
